800c97e2a658e643998b5169548c8334098a066c
Hansin Susatya <hansinsusatya@gmail.com>
2018-10-01 07:57:43 +0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/tutorial.reducer';
+import { ReadComponent } from './components/read/read.component';
+import { CreateComponent } from './components/create/create.component';
+
+@NgModule({
+	declarations: [ AppComponent, ReadComponent, CreateComponent ],
+	imports: [
+		BrowserModule,
+		StoreModule.forRoot({
+			tutorial: reducer
+		})
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
+})
+export class AppModule {}
